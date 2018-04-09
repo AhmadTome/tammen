73e9ab5673197307b029ee1ad5e1979d8b3abaf3
Sohayb Saleh <sohaybsaleh95@gmail.com>
2018-04-09 19:27:49 +0300

    aa

--- app/enter_personalinfo.php
+++ app/enter_personalinfo.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class enter_personalinfo extends Model
 {
-    protected $table = "enter_personal_infos";
     protected $keyType = 'double';
     protected $table = 'enter_personal_infos';
 }
